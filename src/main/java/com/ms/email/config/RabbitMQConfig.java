e245fe740adbb3be75a38ec069529e787c138286
AndreyJustino <andreyjustino2020@gmail.com>
2025-03-09 20:42:37 -0300

    fix: erro por falto de "$" e "{"

--- src/main/java/com/ms/email/config/RabbitMQConfig.java
+++ src/main/java/com/ms/email/config/RabbitMQConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    @Value("broker.queue.email.name")
+    @Value("${broker.queue.email.name}")
     private String queue;
 
     @Bean
